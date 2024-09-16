b678f875442ff77555fff32a6c0ef77b42dcd0bd
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-09-16 16:14:20 +0100

    Xcode project created

---
+++ CryptoCurrencySwiftUI/CryptoCurrencySwiftUI/CryptoCurrencySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencySwiftUIApp.swift
+//  CryptoCurrencySwiftUI
+//
+//  Created by Claire Roughan on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
